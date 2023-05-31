60c5733773882d3ac16f12692a401c5fcba02408
unknown <noorahamdahk@gmail.com>
2023-05-31 13:11:33 +0300

    This is the admain screen with editing

--- lib/pharmacyMangement/admain/presentation/screen/auth/add_admain.dart
+++ lib/pharmacyMangement/admain/presentation/screen/auth/add_admain.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy_system/pharmacyMangement/admain/presentation/screen/auth/home.dart';
+import 'package:pharmacy_system/utils/core/constance/color_constance.dart';
+import 'package:pharmacy_system/utils/widget/basic_controller.dart';
 import 'package:pharmacy_system/utils/widget/text_form_field.dart';
 
 class Admin_Register extends StatefulWidget {
@@ -9,16 +11,34 @@ class Admin_Register extends StatefulWidget {
 
 class _Admin_RegisterState extends State<Admin_Register> {
   int _index = 0;
+  var formKey1 = GlobalKey<FormState>();
+  var formKey2 = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController namePharmacyController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
 
   List<Step> listStep() => [
         Step(
           title: const Text("Account"),
-          content: Step1Widget(),
+          content: Step1Widget(
+            formKey: formKey1,
+            emailController: emailController,
+            nameController: nameController,
+            passwordController: passwordController,
+          ),
           isActive: _index > 0 ? true : false,
         ),
         Step(
           title: const Text("Address"),
-          content: AdressScreen(),
+          content: AdressScreen(
+            namePharmacyController: namePharmacyController,
+            phoneController: phoneController,
+            formKey: formKey2,
+            addressController: addressController,
+          ),
           isActive: _index > 1 ? true : false,
         ),
         Step(
@@ -26,7 +46,6 @@ class _Admin_RegisterState extends State<Admin_Register> {
             content: FinishRegisterScreen(),
             state: StepState.complete),
       ];
-  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -36,50 +55,104 @@ class _Admin_RegisterState extends State<Admin_Register> {
           margin: const EdgeInsets.only(
             top: 20.0,
           ),
-          child: Column(
-            children: [
-              Expanded(
-                child: Stepper(
-                  elevation: 0.0,
-                  physics: const BouncingScrollPhysics(),
-                  // margin: const EdgeInsets.all(5.0),
-                  currentStep: _index,
-                  onStepCancel: () {
-                    if (_index == 0) {
-                      return;
-                    }
-                    setState(() {
-                      _index -= 1;
-                    });
-                  },
-                  onStepTapped: (int index) {
-                    setState(() {
-                      _index = index;
-                    });
-                  },
-                  onStepContinue: () {
-                    if (_index == 2) {
-                      print("hello 1");
-                      // call api register
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeScreen()));
-                      return;
-                    }
-                    setState(() {
-                      _index += 1;
-                    });
-                    ;
-                  },
-                  type: StepperType.vertical,
-                  steps: listStep(),
+          child: Form(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Stepper(
+                    controlsBuilder: (context, ControlsDetails details) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: DefaultContainer(
+                              height: 40.0,
+                              colorContainer: AllColors.greenColor,
+                              borderRadius: 8.0,
+                              text: "Continue",
+                              onPressed: () {
+                               setState(() {
+                                 if(_index == 0){
+                                   if(formKey1.currentState!.validate())
+                                   {
+                                      _index += 1;
+                                   }
+                                 }
+                                 if(_index == 1){
+                                   if(formKey2.currentState!.validate())
+                                   {
+                                     _index += 1;
+                                   }
+                                 }
+                               });
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10.0,
+                          ),
+                          Expanded(
+                            child: DefaultContainer(
+                              height: 40.0,
+                              colorContainer: AllColors.redColor,
+                              borderRadius: 8.0,
+                              text: _index <listStep().length -1 ? "Back" : "Cancel",
+                              onPressed: () {
+                               setState(() {
+                                 if(_index == 2 ){
+                                   _index = 0 ;
+                                 }
+                                 if(_index == 0){
+                                   return ;
+                                 }
+                                 if(_index <listStep().length -1){
+                                   _index -=1 ;
+                                 }
+
+                               });
+                              },
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                    // key: formKey,
+                    elevation: 0.0,
+                    physics: const BouncingScrollPhysics(),
+                    // margin: const EdgeInsets.all(5.0),
+                    currentStep: _index,
+                    onStepCancel: () {
+                      if (_index == 0) {
+                        return;
+                      }
+                      setState(() {
+                        _index -= 1;
+                      });
+                    },
+                    onStepTapped: (int index) {
+                      setState(() {
+                        _index = index;
+                      });
+                    },
+
+                    type: StepperType.vertical,
+                    steps: listStep(),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-            ],
+                DefaultContainer(
+                  text: "Add Admin",
+                  color: Colors.white,
+                  //height: 100.0,
+                  onPressed: () {
+                    // if (formKey.currentState!.validate()) {
+                    //   // GetApi Register
+                    // }
+                  },
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -88,62 +161,86 @@ class _Admin_RegisterState extends State<Admin_Register> {
 }
 
 class Step1Widget extends StatelessWidget {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController;
+
+  final TextEditingController nameController;
+
+  final TextEditingController passwordController;
+  final  Key formKey ;
+  const Step1Widget({
+    super.key,
+    required this.emailController,
+    required this.nameController,
+    required this.passwordController,
+    required this.formKey,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DefaultFormField(
-              hint: "Name",
-              controller: nameController,
-              type: TextInputType.text,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Name";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 39.0,
-            ),
-            DefaultFormField(
-              hint: "Email",
-              controller: emailController,
-              prefixIcon: Icons.email,
-              type: TextInputType.emailAddress,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Email";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 39.0,
-            ),
-            DefaultFormField(
-              hint: "Password",
-              controller: passwordController,
-              obscure: true,
-              prefixIcon: Icons.lock,
-              type: TextInputType.visiblePassword,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Password";
-                }
-                return null;
-              },
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 25.0,
+              ),
+              const Text("Name"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: nameController,
+                type: TextInputType.text,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Name";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              const Text("Email"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: emailController,
+                type: TextInputType.emailAddress,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Email";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              const Text("Password"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: passwordController,
+                obscure: true,
+                type: TextInputType.visiblePassword,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Password";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30.0,),
+            ],
+          ),
         ),
       ),
     );
@@ -151,61 +248,84 @@ class Step1Widget extends StatelessWidget {
 }
 
 class AdressScreen extends StatelessWidget {
-  TextEditingController namePharmacyController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
-  TextEditingController addressController = TextEditingController();
+  final TextEditingController namePharmacyController;
+  final TextEditingController phoneController;
+
+  final TextEditingController addressController;
+  final Key formKey ;
+  const AdressScreen({
+    super.key,
+    required this.namePharmacyController,
+    required this.phoneController,
+    required this.addressController,
+    required this.formKey,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DefaultFormField(
-              hint: "Name Pharmacy",
-              controller: namePharmacyController,
-              type: TextInputType.text,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Name Pharmayc";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 39.0,
-            ),
-            DefaultFormField(
-              hint: "Address",
-              controller: addressController,
-              prefixIcon: Icons.location_on_sharp,
-              type: TextInputType.text,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Location";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 39.0,
-            ),
-            DefaultFormField(
-              hint: "Phone Number",
-              controller: phoneController,
-              prefixIcon: Icons.phone,
-              type: TextInputType.phone,
-              validate: (String? value) {
-                if (value!.isEmpty) {
-                  return "Enter Your Phone";
-                }
-                return null;
-              },
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text("Name Pharmacy"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: namePharmacyController,
+                type: TextInputType.text,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Name Pharmayc";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              const Text("Address"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: addressController,
+                type: TextInputType.text,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Location";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              const Text("Phone Number"),
+              const SizedBox(
+                height: 15.0,
+              ),
+              DefaultFormField(
+                controller: phoneController,
+                type: TextInputType.phone,
+                validate: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Enter Your Phone";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 25.0,),
+            ],
+          ),
         ),
       ),
     );
@@ -217,21 +337,21 @@ class FinishRegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Stack(
-        children:  [
+        children: [
           const CircleAvatar(
             radius: 180.0,
-            backgroundColor: Colors.blue,
+            backgroundColor: AllColors.appColor,
           ),
           Positioned(
             top: 280.0,
             left: 200.0,
             bottom: 30.0,
-            child: IconButton (
+            child: IconButton(
               onPressed: () {},
               icon: const Icon(
                 Icons.camera_alt_rounded,
                 size: 40.0,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
           )
---
+++ lib/utils/core/constance/color_constance.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AllColors {
+
+  static const  Color appColor = Color(0xff0F969C) ;
+  static const  Color greenColor = Color(0xff00FD38) ;
+  static const  Color redColor = Color(0xffFC0000) ;
+}
